189649302486bd338988bbdca78fa2f7ece2f244
Forrest Bingham <bingham_5@live.com>
2020-02-07 14:17:54 -0700

    Updated tests

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const helmet = require('helmet');
 
 //Include routers/middlewares
 const authRouter = require('../auth/auth-router.js');
+
 const usersRouter = require('../users/users-router.js');
 const sleepLogRouter = require('../sleepLog/sleepLog-router.js');
 const server = express();
@@ -15,6 +16,8 @@ server.use(express.json());
 server.get("/", (req, res) => {
     res.status(200).json({ test:"Success" });
   });
+
+
 
   //Server.use
 
--- sleepLog/sleepLog-model.test.js
+++ sleepLog/sleepLog-model.test.js
@@ -18,7 +18,7 @@ describe('SleepLog mode', function(){
 
         it('Adds a new entry to the sleep log', async function(){
             
-            SleepLog.add({date: '03/02/2020', wakeUpRating: 2, dayRating: 2, nightRating: 2, wokeup: "8:30 AM", fellAsleep: "11:00 PM", totalTimeSlept:"8:00", user_id:1})
+            SleepLog.add({date: '03/02/2020',timeCreated: "8:00 AM", wakeUpRating: 2, dayRating: 2, nightRating: 2, wokeup: "8:30 AM", fellAsleep: "11:00 PM", totalTimeSlept:"8:00", user_id:1})
         
             const log = await db('sleepLog');
             expect(log).toHaveLength(1);
@@ -34,8 +34,8 @@ describe('SleepLog mode', function(){
 
         it('removes an existing entry to the sleep log', async function(){
 
-            await SleepLog.add({ date: '03/02/2020', wakeUpRating: 2, dayRating: 2, nightRating: 2, wokeup: "8:30 AM", fellAsleep: "11:00 PM", totalTimeSlept:"8:00", user_id:1})
-            await  SleepLog.add({ date: '03/03/2020', wakeUpRating: 3, dayRating: 3, nightRating: 3, wokeup: "8:30 AM", fellAsleep: "12:00 PM", totalTimeSlept:"8:30", user_id:1})
+            await SleepLog.add({ date: '03/02/2020',timeCreated: "8:00 AM", wakeUpRating: 2, dayRating: 2, nightRating: 2, wokeup: "8:30 AM", fellAsleep: "11:00 PM", totalTimeSlept:"8:00", user_id:1})
+            await  SleepLog.add({ date: '03/03/2020',timeCreated: "8:00 AM", wakeUpRating: 3, dayRating: 3, nightRating: 3, wokeup: "8:30 AM", fellAsleep: "12:00 PM", totalTimeSlept:"8:30", user_id:1})
             await SleepLog.remove(1);
 
 
@@ -52,9 +52,9 @@ describe('SleepLog mode', function(){
 
         it('updates an existing entry in the sleep log', async function(){
 
-            await SleepLog.add({ date: '03/02/2020', wakeUpRating: 2, dayRating: 2, nightRating: 2, wokeup: "8:30 AM", fellAsleep: "11:00 PM", totalTimeSlept:"8:00", user_id:1})
+            await SleepLog.add({ date: '03/02/2020', timeCreated: "8:00 AM", wakeUpRating: 2, dayRating: 2, nightRating: 2, wokeup: "8:30 AM", fellAsleep: "11:00 PM", totalTimeSlept:"8:00", user_id:1})
 
-            const changes = {date: '03/02/2020', wakeUpRating: 1, dayRating: 1, nightRating: 1, wokeup: "8:30 AM", fellAsleep: "11:00 PM", totalTimeSlept:"8:00", user_id:1}
+            const changes = {date: '03/02/2020', timeCreated: "8:00 AM", wakeUpRating: 1, dayRating: 1, nightRating: 1, wokeup: "8:30 AM", fellAsleep: "11:00 PM", totalTimeSlept:"8:00", user_id:1}
 
             await SleepLog.update(1,changes);
 
